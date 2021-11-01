4893b855350b764527b0cd94fa22d7ed93298207
Dhruv Chaturvedi <dc2020@hw.ac.uk>
2021-11-01 17:25:17 +0400

    progress until bee class creation

--- game.js
+++ game.js
@@ -22,32 +22,6 @@ function Bear() {
         this.htmlElement.style.display = "block";
     };
 
-    function start() {
-        //create bear
-        bear = new Bear();
-        document.addEventListener("keydown", moveBear, false);
-    }
-
-    function moveBear(e) {
-        //codes of the four keys
-        const KEYUP = 38;
-        const KEYDOWN = 40;
-        const KEYLEFT = 37;
-        const KEYRIGHT = 39;
-        if (e.keyCode == KEYRIGHT) {
-        bear.move(1, 0)
-        } // right key
-        if (e.keyCode == KEYLEFT) {
-        bear.move(-1, 0)
-        } // left key
-        if (e.keyCode == KEYUP) {
-        bear.move(0, -1)
-        } // up key
-        if (e.keyCode == KEYDOWN) {
-        bear.move(0, 1)
-        } // down key
-    }
-
     this.fitBounds = function() {
         let parent = this.htmlElement.parentElement;
         let iw = this.htmlElement.offsetWidth;
@@ -67,8 +41,82 @@ function Bear() {
         this.x += this.dBear * xDir;
         this.y += this.dBear * yDir;
         this.display();
-        };
-       
-
+        };  
 }
+
+function setSpeed() {
+    bear.dBear = parseInt(document.getElementById("spBear").value);
+}
+
+function start() {
+    //create bear
+    bear = new Bear();
+    document.addEventListener("keydown", moveBear, false);
+    document.getElementById("spBear").addEventListener("change", setSpeed);
+}
+
+function moveBear(e) {
+    //codes of the four keys
+    const KEYUP = 38;
+    const KEYDOWN = 40;
+    const KEYLEFT = 37;
+    const KEYRIGHT = 39;
+    if (e.keyCode == KEYRIGHT) {
+    bear.move(1, 0)
+    } // right key
+    if (e.keyCode == KEYLEFT) {
+    bear.move(-1, 0)
+    } // left key
+    if (e.keyCode == KEYUP) {
+    bear.move(0, -1)
+    } // up key
+    if (e.keyCode == KEYDOWN) {
+    bear.move(0, 1)
+    } // down key
+}
+
+class Bee {
+    constructor(beeNumber) {
+        //the HTML element corresponding to the IMG of the bee
+        this.htmlElement = createBeeImg(beeNumber);
+        //iits HTML ID
+        this.id = this.htmlElement.id;
+        //the left position (x)
+        this.x = this.htmlElement.offsetLeft;
+        //the top position (y)
+        this.y = this.htmlElement.offsetTop;
+        this.move = function(dx, dy) {
+            //move the bees by dx, dy
+            this.x += dx;
+            this.y += dy;
+            this.display();
+        };
+        this.display = function() {
+            //adjust position of bee and display it
+            this.fitBounds();//add this to adjust to bounds
+            this.htmlElement.style.left = this.x + "px";
+            this.htmlElement.style.top = this.y + "px";
+            this.htmlElement.style.display = "block";
+        };
+        this.fitBounds = function() {
+        //check and make sure the bees stays in the board space
+            let parent = this.htmlElement.parentElement;
+            let iw = this.htmlElement.offsetWidth;
+            let ih = this.htmlElement.offsetHeight;
+            let l = parent.offsetLeft;
+            let t = parent.offsetTop;
+            let w = parent.offsetWidth;
+            let h = parent.offsetHeight;
+            if (this.x < 0)
+            this.x = 0;
+            if (this.x > w - iw)
+            this.x = w - iw;
+            if (this.y < 0)
+            this.y = 0;
+            if (this.y > h - ih)
+            this.y = h - ih;
+        };
+    }
+}
+   
    
